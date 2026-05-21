f9b731d682c54561cb6741d4cf97bc756c4191b7
SungHyun SHIN <brainshin@hotmail.com>
2026-05-21 20:59:02 +0900

    Update instance.tf

--- packer-demo/instance.tf
+++ packer-demo/instance.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "example" {
   ami           = var.AMI_ID
-  instance_type = "t2.micro"
+  instance_type = "t4g.micro"
 
   # the VPC subnet
   subnet_id = aws_subnet.main-public-1.id
